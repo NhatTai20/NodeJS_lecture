ef1da12df07e765731c22e4287e7fe1d1855cd55
NhatTai20 <ntai20111@gmail.com>
2021-05-23 00:14:29 +0700

    up to lesson 36, 1 more less

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ const path = require('path');
 const morgan = require('morgan');
 const db =require('./config/db');
 const route = require('./routes');
-
+const methodOverride = require('method-override')
+const SortMiddleware = require('./app/middleware/sortMiddleware');
 //connect to db
 db.connect();
 
@@ -16,12 +17,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+app.use(SortMiddleware);
 //app.use(morgan('combined'));
 
 app.engine(
     'hbs',
     handlebars({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
+                const icons = {
+                    default: 'oi oi-elevator',
+                    asc: 'oi oi-sort-ascending',
+                    desc: 'oi oi-sort-descending'
+                }
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc'
+                }
+                const icon = icons[sortType];
+                const type = types[sortType];
+                return `<a href="?_sort&column=${field}&type=${type}">
+                <span class="${icon}"></span>
+                </a>`
+            }
+        }
     }),
 );
 app.set('view engine', 'hbs');
